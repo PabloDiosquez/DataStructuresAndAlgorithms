a2b974ea95a31bf644f1297c4514ab2b4de0500d
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-08 16:04:11 -0300

    Add LinkedList

--- DataStructuresAndAlgorithms/src/com/LinkedList/LinkedList.java
+++ DataStructuresAndAlgorithms/src/com/LinkedList/LinkedList.java
@@ -3,24 +3,33 @@ package com.LinkedList;
 public class LinkedList {
     private Node head;
 
-    // Methods
-    public void insert(int data){
+    // Constructor
+    public LinkedList() {
+        // Initialize an empty linked list with a null head
+        this.head = null;
+    }
+
+    // Method to insert a new node at the end of the linked list
+    public void insert(int data) {
         Node node = new Node(data, null);
 
-        if(this.head == null){
+        if (this.head == null) {
+            // If the list is empty, set the new node as the head
             this.head = node;
-        }
-        else {
+        } else {
+            // Traverse to the end and add the new node
             Node temp = this.head;
-            while(temp.getNext() != null){
+            while (temp.getNext() != null) {
                 temp = temp.getNext();
             }
             temp.setNext(node);
         }
     }
 
+    // Method to insert a new node at a specific index in the linked list
     public void insertAt(int index, int data) throws Exception {
         if (index < 0) {
+            // Throw an exception if the index is negative
             throw new Exception("Index cannot be negative.");
         }
 
@@ -28,13 +37,15 @@ public class LinkedList {
         Node prev = null;
         Node current = this.head;
 
-        while(current != null && currentIndex < index){
+        // Traverse to the specified index or the end of the list
+        while (current != null && currentIndex < index) {
             prev = current;
             current = current.getNext();
             currentIndex++;
         }
 
         if (currentIndex < index) {
+            // Throw an exception if the index is out of bounds
             throw new Exception("Index out of bounds.");
         }
 
@@ -42,41 +53,55 @@ public class LinkedList {
             // Insert at the beginning of the list
             this.head = new Node(data, this.head);
         } else {
+            // Insert at the specified index
             prev.setNext(new Node(data, current));
         }
     }
 
+    // Method to insert a new node at the beginning of the linked list
     public void insertAtStart(int data) throws Exception {
+        // Wrapper method to insert at index 0
         this.insertAt(0, data);
     }
 
+    // Method to delete a node at a specific index in the linked list
     public int delete(int index) throws Exception {
-        if(index < 0) throw new Exception("Index cannot be negative");
+        if (index < 0) {
+            // Throw an exception if the index is negative
+            throw new Exception("Index cannot be negative");
+        }
 
         int currentIndex = 0;
         Node prev = null;
         Node current = this.head;
 
-        while(current != null && currentIndex < index){
+        // Traverse to the specified index or the end of the list
+        while (current != null && currentIndex < index) {
             prev = current;
             current = current.getNext();
             currentIndex++;
         }
 
-        if(currentIndex < index) throw new Exception("Index out of bounds");
-        else {
+        if (currentIndex < index) {
+            // Throw an exception if the index is out of bounds
+            throw new Exception("Index out of bounds");
+        } else {
             if (prev != null) {
+                // Delete the node at the specified index
                 prev.setNext(current.getNext());
                 return current.getData();
             } else {
+                // Delete the first node when the list is not empty
                 this.head = current.getNext();
                 return this.head.getData();
             }
         }
     }
-    public void show(){
+
+    // Method to display the elements of the linked list
+    public void show() {
         Node node = this.head;
-        while(node != null){
+        while (node != null) {
             System.out.println(node);
             node = node.getNext();
         }
--- DataStructuresAndAlgorithms/src/com/LinkedList/Main.java
+++ DataStructuresAndAlgorithms/src/com/LinkedList/Main.java
@@ -1,26 +1,43 @@
 package com.LinkedList;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        LinkedList list = new LinkedList();
-
-        list.insert(12);
-        list.insert(14);
-        list.insert(-1);
-        list.insert(10);
-        list.insert(8);
+    public static void main(String[] args) {
+        // Create a new linked list
+        LinkedList linkedList = new LinkedList();
 
         try {
-            list.insertAt(5, 23);
+            // Insert elements into the linked list
+            linkedList.insert(10);
+            linkedList.insert(20);
+            linkedList.insert(30);
+
+            // Display the initial linked list
+            System.out.println("Initial Linked List:");
+            linkedList.show();
+
+            // Insert a new element at index 1
+            linkedList.insertAt(1, 15);
+
+            // Display the linked list after insertion
+            System.out.println("\nLinked List after Insertion at Index 1:");
+            linkedList.show();
+
+            // Insert a new element at the beginning
+            linkedList.insertAtStart(5);
+
+            // Display the linked list after insertion at the beginning
+            System.out.println("\nLinked List after Insertion at the Beginning:");
+            linkedList.show();
+
+            // Delete the element at index 2
+            int deletedData = linkedList.delete(2);
+            System.out.println("\nDeleted Element: " + deletedData);
+
+            // Display the linked list after deletion
+            System.out.println("\nLinked List after Deletion at Index 2:");
+            linkedList.show();
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            System.out.println("Exception: " + e.getMessage());
         }
-
-        list.show();
-
-        list.delete(2);
-
-        System.out.println();
-        list.show();
     }
 }
