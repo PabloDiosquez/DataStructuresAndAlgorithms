1a7f657290a611127ac6a3aeb511393040d4a807
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-09 22:14:51 -0300

    Add Linear Search

--- DataStructuresAndAlgorithms/src/com/LinearSearch/Main.java
+++ DataStructuresAndAlgorithms/src/com/LinearSearch/Main.java
@@ -1,18 +1,74 @@
 package com.LinearSearch;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        int[] nums = {23, 12, 56, 75, 11, 14};
-        int target = 75;
+    public static void main(String[] args) {
+        int[] numbers = {23, 12, 56, 75, 11, 14};
+        int targetValue = 101;
 
-        System.out.printf("Index of %s: %s%n", target, linearSearch(nums, 75));
+        try {
+            // Perform linear search and print the result
+            int resultIndex = performLinearSearch(numbers, targetValue);
+            System.out.printf("Index of %d: %d%n", targetValue, resultIndex);
+        } catch (Exception e) {
+            // Handle any exceptions that might occur during the search
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
-    static int linearSearch(int[] numbers, int target) throws Exception {
-        if(numbers == null) throw new Exception("The array is null.");
-        for(int index = 0; index < numbers.length; index++){
-            if(numbers[index] == target) return index;
+    // Method for checking if the array contains the target value
+    static boolean containsTarget(int[] array, int target) throws Exception {
+        // Check if the array is null, and throw an exception if it is
+        if (array == null) {
+            throw new Exception("The array is null.");
         }
-        return -1;
+
+        // Iterate through the array elements
+        for (int number : array) {
+            // If the target is found, return true
+            if (number == target) {
+                return true;
+            }
+        }
+
+        // If the target is not found, return false
+        return false;
+    }
+
+    // Method for finding the target value in the array
+    static int findTarget(int[] array, int target) throws Exception {
+        // Check if the array is null, and throw an exception if it is
+        if (array == null) {
+            throw new Exception("The array is null.");
+        }
+
+        // Iterate through the array elements
+        for (int index = 0; index < array.length; index++) {
+            // If the target is found, return the index
+            if (array[index] == target) {
+                return index;
+            }
+        }
+
+        // If the target is not found, throw an exception
+        throw new Exception("The target is not in the array of numbers.");
+    }
+
+    // Method for linear search
+    static int performLinearSearch(int[] array, int target) throws Exception {
+        // Check if the array is null, and throw an exception if it is
+        if (array == null) {
+            throw new Exception("The array is null.");
+        }
+
+        // Iterate through the array elements
+        for (int index = 0; index < array.length; index++) {
+            // If the target is found, return the index
+            if (array[index] == target) {
+                return index;
+            }
+        }
+
+        // If the target is not found, throw an exception
+        throw new Exception("The target is not in the array of numbers.");
     }
 }
