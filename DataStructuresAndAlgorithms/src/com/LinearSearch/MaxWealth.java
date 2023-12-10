bab1add58dfee79b8c0d18eff9ac0a50f2143c35
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-10 20:05:05 -0300

    Add Linear Search

--- DataStructuresAndAlgorithms/src/com/LinearSearch/MaxWealth.java
+++ DataStructuresAndAlgorithms/src/com/LinearSearch/MaxWealth.java
@@ -4,24 +4,40 @@ import java.util.Arrays;
 
 public class MaxWealth {
     public static void main(String[] args) {
-        System.out.println(maximumWealth(new int[][] {
-                {1,263},
-                {2,4,50,},
+        // Example usage of maximumWealth function with a 2D array representing bank accounts
+        int[][] bankAccounts = {
+                {1, 26},
+                {2, 4, 50},
                 {10, 20}
-        }));
+        };
+
+        // Calculate and print the maximum wealth among all persons
+        System.out.println("Maximum Wealth: " + maximumWealth(bankAccounts));
     }
 
-    static int maximumWealth(int[][] accounts){
-        int maximumWealth = sum(accounts[0]);
-        for (int person = 1; person < accounts.length; person++){
-            if(maximumWealth < sum(accounts[person]))
-                maximumWealth = sum(accounts[person]);
+    // Function to find the maximum wealth among all persons
+    static int maximumWealth(int[][] accounts) {
+        // Initialize maximumWealth with the wealth of the first person
+        int maximumWealth = total_wealth_of_a_person(accounts[0]);
+
+        // Iterate through each person's accounts to find the maximum wealth
+        for (int person = 1; person < accounts.length; person++) {
+            int currentWealth = total_wealth_of_a_person(accounts[person]);
+
+            // Update maximumWealth if the current person has more wealth
+            if (maximumWealth < currentWealth) {
+                maximumWealth = currentWealth;
+            }
         }
         return maximumWealth;
     }
-    static int sum(int[] person){
+
+    // Function to calculate the total wealth of a person
+    static int total_wealth_of_a_person(int[] person) {
         int totalWealth = 0;
-        for(int account = 0; account < person.length; account++){
+
+        // Iterate through each account to calculate total wealth
+        for (int account = 0; account < person.length; account++) {
             totalWealth += person[account];
         }
         return totalWealth;
