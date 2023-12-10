7250bf32bffcf65ab4f4af6edcf30dce48f47de7
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-10 19:28:36 -0300

    Add Linear Search

--- DataStructuresAndAlgorithms/src/com/LinearSearch/EvenDigits.java
+++ DataStructuresAndAlgorithms/src/com/LinearSearch/EvenDigits.java
@@ -3,7 +3,7 @@ package com.LinearSearch;
 public class EvenDigits {
     public static void main(String[] args) {
         // Initialize an array of integers
-        int[] numbers = {12, 3, 45, 12, 654233};
+        int[] numbers = {12, 3123, 0, -12, 654233};
 
         // Call the findNumbers method and store the result in the 'even' variable
         int even = findNumbers(numbers);
@@ -30,22 +30,34 @@ public class EvenDigits {
     static boolean even(int number){
         // Using the digits method to get the number of digits
         // and checking if it is even
-        return digits(number) % 2 == 0;
+        return digitsV2(number) % 2 == 0;
     }
 
     // Method to count the number of digits in a number
     static int digits(int number){
         // Initializing a counter for the number of digits
         int numberOfDigits = 0;
-
+        if (number == 0) return ++numberOfDigits;
         // Looping through each digit by dividing the number by 10
         // until the number becomes 0
-        while(number > 0){
+        while(Math.abs(number) > 0){
             numberOfDigits++;
             number = number / 10;
         }
-
         // Returning the count of digits
         return numberOfDigits;
+    }
+
+    // Method to count the number of digits in a given number (version 2)
+    static int digitsV2(int number) {
+        // Check if the number is zero, in which case it has one digit
+        if (number == 0) {
+            return 1;
+        }
+
+        // Calculate the number of digits using the logarithm function
+        // The Math.abs(number) ensures that the input is non-negative
+        // The expression (int)Math.log10(Math.abs(number)) + 1 calculates the count of digits
+        return (int) Math.log10(Math.abs(number)) + 1;
     }
 }
