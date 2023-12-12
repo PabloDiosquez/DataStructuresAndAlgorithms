234cdec68f142b0e6cfb8465efce88ff54d903c1
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-12 11:59:51 -0300

    Add Floor

--- DataStructuresAndAlgorithms/src/com/BinarySearch/Floor.java
+++ DataStructuresAndAlgorithms/src/com/BinarySearch/Floor.java
@@ -6,6 +6,37 @@ public class Floor {
     }
 
     // Floor algorithm implementation for a sorted array
+    static int floor(int[] sortedArr, int target) throws Exception {
+        // Check if the array is empty
+        if (sortedArr.length == 0) throw new Exception("The array is empty.");
+
+        // Initialize pointers for binary search
+        int start = 0;
+        int end = sortedArr.length - 1;
+        int middle = 0;
+
+        // Binary search loop
+        while (start <= end) {
+            // Calculate the middle index using integer division
+            middle = start + (end - start) / 2;
+
+            // Binary Search Logic:
+            if (sortedArr[middle] == target)
+                return target;  // Found an exact match, return the target
+
+            else if (sortedArr[middle] > target)
+                end = middle - 1;  // Move to the left half
+
+            else
+                start = middle + 1;  // Move to the right half
+        }
+
+        // If the loop ends, middle points to the largest element less than the target
+        // The value at start represents the floor value
+        return sortedArr[start];
+    }
+
+    // Floor algorithm implementation for a sorted array
     static int floorV2(int[] sortedArr, int target) throws Exception {
         // Check if the array is empty
         if (sortedArr.length == 0) throw new Exception("The array is empty.");
