92c1423b9d9783e296853cd02c374be2c938bb56
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-13 15:35:13 -0300

    Add Letters

--- DataStructuresAndAlgorithms/src/com/BinarySearch/Letters.java
+++ DataStructuresAndAlgorithms/src/com/BinarySearch/Letters.java
@@ -4,38 +4,38 @@ public class Letters {
     public static void main(String[] args) {
         // Example usage of indexOfSmallestLetterGreaterThan
         char[] sortedArray1 = {'a', 'c', 'e', 'g', 'i'};
-        char target1 = 'f';
+        char target1 = 'h';
         try {
-            int result1 = indexOfSmallestLetterGreaterThan(sortedArray1, target1);
-            System.out.println("Index of the smallest letter greater than '" + target1 + "': " + result1);
+            char result1 = nextGreatestLetter(sortedArray1, target1);
+            System.out.println("Smallest letter greater than '" + target1 + "': " + result1);
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
 
         // Example usage of indexOfSmallestLetterGreaterThanV2
         char[] sortedArray2 = {'b', 'd', 'f', 'h', 'j'};
-        char target2 = 'i';
+        char target2 = 'z';
         try {
-            int result2 = indexOfSmallestLetterGreaterThanV2(sortedArray2, target2);
-            System.out.println("Index of the smallest letter greater than '" + target2 + "': " + result2);
+            char result2 = nextGreatestLetterV2(sortedArray2, target2);
+            System.out.println("Smallest letter greater than '" + target2 + "': " + result2);
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
     }
 
     /**
-     * Finds the index of the smallest letter in a sorted array that is greater than the target letter.
+     * Finds the smallest letter in a sorted array that is greater or equal than the target letter.
      * @param sortedArray The sorted array of characters.
      * @param target The target letter.
      * @return The index of the smallest letter greater than the target letter.
      * @throws Exception If the array of characters is empty.
      */
-    static int indexOfSmallestLetterGreaterThan(char[] sortedArray, char target) throws Exception {
+    static char nextGreatestLetter(char[] sortedArray, char target) throws Exception {
         // Check if the array of characters is empty
         if (sortedArray.length == 0) throw new Exception("The array of characters is empty.");
 
         // Check if the largest element in the array is smaller than the target
-        if (sortedArray[sortedArray.length - 1] < target) return -1;
+        if (sortedArray[sortedArray.length - 1] < target) return sortedArray[0];
 
         // Initialize variables for linear search
         int start = 0;
@@ -45,26 +45,26 @@ public class Letters {
         // Linear search loop
         while (start <= end) {
             middle = start + (end - start) / 2;
-            if (sortedArray[middle] == target) return middle;
+            if (sortedArray[middle] == target) return sortedArray[middle++];
             else if (sortedArray[middle] < target) start = middle + 1;
             else end = middle - 1;
         }
-        return start;
+        return sortedArray[start];
     }
 
     /**
-     * Finds the index of the smallest letter in a sorted array that is greater than the target letter using binary search.
+     * Finds the smallest letter in a sorted array that is greater or equal than the target letter using binary search.
      * @param sortedArray The sorted array of characters.
      * @param target The target letter.
      * @return The index of the smallest letter greater than the target letter.
      * @throws Exception If the array of characters is empty.
      */
-    static int indexOfSmallestLetterGreaterThanV2(char[] sortedArray, char target) throws Exception {
+    static char nextGreatestLetterV2(char[] sortedArray, char target) throws Exception {
         // Check if the array of characters is empty
         if (sortedArray.length == 0) throw new Exception("The array of characters is empty.");
 
         // Check if the largest element in the array is smaller than the target
-        if (sortedArray[sortedArray.length - 1] < target) return -1;
+        if (sortedArray[sortedArray.length - 1] < target) return sortedArray[0];
 
         // Initialize variables for binary search
         int start = 0;
@@ -85,6 +85,6 @@ public class Letters {
         }
 
         // Return the index of the smallest letter greater than the target
-        return middle;
+        return sortedArray[middle];
     }
 }
