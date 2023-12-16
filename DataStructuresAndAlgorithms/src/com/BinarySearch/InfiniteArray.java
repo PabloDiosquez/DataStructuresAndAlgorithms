aedeeaed5bd061b865801dcfd42dd06276a2da5e
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-16 18:39:45 -0300

    Add InfiniteArray

--- DataStructuresAndAlgorithms/src/com/BinarySearch/InfiniteArray.java
+++ DataStructuresAndAlgorithms/src/com/BinarySearch/InfiniteArray.java
@@ -2,8 +2,8 @@ package com.BinarySearch;
 
 public class InfiniteArray {
     public static void main(String[] args) {
-        int[] arr = new int[]{1,2,3,5,7,8,9,10,12,14,16,19,20,22};
-        int target = 16;
+        int[] arr = {1, 2, 3, 4, 6, 8, 12, 14, 15, 16, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58};
+        int target = 30;
 
         System.out.println(findIndex(arr, target));
     }
@@ -11,16 +11,17 @@ public class InfiniteArray {
     static int findIndex(int[] sortedArray, int target){
         int start  = 0;
         int end    = 1;
-        int middle = 0;
 
         while(true){
-            if(sortedArray[end] <= target) end = end * 2;
-            else break;
+            if(sortedArray[start] <= target && target <= sortedArray[end])
+                break;
+            start = end + 1;
+            end = end * 2;
         }
 
         // Binary Search
         while(start <= end){
-            middle = start + (end - start) / 2;
+            int middle = start + (end - start) / 2;
             if(sortedArray[middle] == target) return middle;
             else if(sortedArray[middle] < target) start = middle + 1;
             else end = middle - 1;
