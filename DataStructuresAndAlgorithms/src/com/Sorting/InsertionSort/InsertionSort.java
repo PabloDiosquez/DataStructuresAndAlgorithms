317153c77b4ca6faeac8a4ed05a859c451aadc91
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-18 18:42:43 -0300

    Add Insertion Sort

---
+++ DataStructuresAndAlgorithms/src/com/Sorting/InsertionSort/InsertionSort.java
@@ -0,0 +1,26 @@
+package com.Sorting.InsertionSort;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] nums = {0, -12, 3, 1, -18, 23};
+        System.out.println(Arrays.toString(nums));
+        insertion(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    static void insertion(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if(arr[j] < arr[j-1]) swap(arr, j, j-1);
+                else break;
+            }
+        }
+    }
+    static void swap(int[] arr, int first, int second) {
+        int temp    = arr[first];
+        arr[first]  = arr[second];
+        arr[second] = temp;
+    }
+}
