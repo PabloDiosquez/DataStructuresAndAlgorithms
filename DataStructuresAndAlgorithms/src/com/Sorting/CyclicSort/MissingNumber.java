5d24d838ee59ccac0cf900531a9f2d7aa3048f20
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-19 11:55:57 -0300

    Add MissingNumber

--- DataStructuresAndAlgorithms/src/com/Sorting/CyclicSort/MissingNumber.java
+++ DataStructuresAndAlgorithms/src/com/Sorting/CyclicSort/MissingNumber.java
@@ -1,38 +1,34 @@
 package com.Sorting.CyclicSort;
 
-// Given an array 'nums' containing n distinct numbers in the range [0, n], return
+// Given an array 'numbers' containing n distinct numbers in the range [0, n], return
 // the only number in the range that is missing from the array.
 // Could you implement a solution using only O(1) extra space in memory
 // and O(n) runtime complexity?
 
 public class MissingNumber {
     public static void main(String[] args) {
-        int[] range = {0,2};
-        System.out.println(missingNumber(range));
+        int[] range = {0, 2, 3, 4};
+        System.out.println("Missing Number: " + missingNumber(range));
     }
 
-    static int missingNumber(int[] range){
-        cyclicSort(range);
+    static int missingNumber(int[] numbers){
+        cyclicSort(numbers);
         int j = 0;
-        while(j < range.length){
-            if(range[j] != j) break;
+        while(j < numbers.length){
+            if(numbers[j] != j) break;
             j++;
         }
         return j;
     }
 
-    static void cyclicSort(int[] range){
+    static void cyclicSort(int[] numbers){
         int i = 0;
-        while(i < range.length){
-            int correct = range[i];
-            if(range[i] == range.length){
-                i++;
+        while(i < numbers.length){
+            int correct = numbers[i]; // 3
+            if(numbers[i] < numbers.length && numbers[i] != numbers[correct]){
+                swap(numbers, i, correct);
             } else {
-                if(range[i] != range[correct]){
-                    swap(range, i, correct);
-                } else {
-                    i++;
-                }
+                i++;
             }
         }
     }
