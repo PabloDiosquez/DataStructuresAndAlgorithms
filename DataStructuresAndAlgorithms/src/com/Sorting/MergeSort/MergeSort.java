b96ea4f4c91b21cf154fc984aab9dd62c8183e7a
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-20 16:38:53 -0300

    Add MergeSort

--- DataStructuresAndAlgorithms/src/com/Sorting/MergeSort/MergeSort.java
+++ DataStructuresAndAlgorithms/src/com/Sorting/MergeSort/MergeSort.java
@@ -2,28 +2,61 @@ package com.Sorting.MergeSort;
 
 import java.util.Arrays;
 
+import java.util.Arrays;
+
+/**
+ * The MergeSort class implements the merge sort algorithm to sort an array of integers.
+ */
 public class MergeSort {
+
+    /**
+     * The main method demonstrates the usage of the mergeSort function.
+     * It initializes an array, performs the merge sort, and prints the sorted array.
+     *
+     * @param args The command-line arguments (not used in this program).
+     */
     public static void main(String[] args) {
+        // Input array to be sorted
         int[] arr = {8, 1, 4, 10, 2, -1};
-        System.out.println(Arrays.toString(mergeSort(arr)));
+
+        // Perform merge sort and print the result
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        int[] sortedArr = mergeSort(arr);
+        System.out.println("Sorted Array: " + Arrays.toString(sortedArr));
     }
 
-    static int[] mergeSort(int[] arr){
-        if(arr.length == 1) return arr;
+    /**
+     * Recursively applies the merge sort algorithm to sort the given array.
+     *
+     * @param arr The array to be sorted.
+     * @return The sorted array.
+     */
+    static int[] mergeSort(int[] arr) {
+        // Base case: if the array has only one element, it is already sorted
+        if (arr.length == 1) return arr;
 
+        // Split the array into two halves and recursively merge sort each half
         int middle = arr.length / 2;
         int[] left = mergeSort(Arrays.copyOfRange(arr, 0, middle));
         int[] right = mergeSort(Arrays.copyOfRange(arr, middle, arr.length));
 
+        // Merge the sorted halves
         return merge(left, right);
     }
 
-    static int[] merge(int[] sortedArr1, int[] sortedArr2){
+    /**
+     * Merges two sorted arrays into a single sorted array.
+     *
+     * @param sortedArr1 The first sorted array.
+     * @param sortedArr2 The second sorted array.
+     * @return The merged and sorted array.
+     */
+    static int[] merge(int[] sortedArr1, int[] sortedArr2) {
         int[] sorted = new int[sortedArr1.length + sortedArr2.length];
 
         int i = 0, j = 0, k = 0;
-        while(i < sortedArr1.length && j < sortedArr2.length){
-            if(sortedArr1[i] < sortedArr2[j]){
+        while (i < sortedArr1.length && j < sortedArr2.length) {
+            if (sortedArr1[i] < sortedArr2[j]) {
                 sorted[k] = sortedArr1[i];
                 i++;
             } else {
@@ -33,17 +66,19 @@ public class MergeSort {
             k++;
         }
 
-        while(i < sortedArr1.length){
+        // Copy remaining elements from both arrays, if any
+        while (i < sortedArr1.length) {
             sorted[k] = sortedArr1[i];
             i++;
             k++;
         }
 
-        while(j < sortedArr2.length){
+        while (j < sortedArr2.length) {
             sorted[k] = sortedArr2[j];
             j++;
             k++;
         }
+
         return sorted;
     }
 }
