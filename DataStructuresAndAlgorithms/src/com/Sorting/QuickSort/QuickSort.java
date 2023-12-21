cb4ef652b01449787133983aac918b4b7f464200
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-21 18:44:04 -0300

    Add QuickSort

--- DataStructuresAndAlgorithms/src/com/Sorting/QuickSort/QuickSort.java
+++ DataStructuresAndAlgorithms/src/com/Sorting/QuickSort/QuickSort.java
@@ -2,43 +2,84 @@ package com.Sorting.QuickSort;
 
 import java.util.Arrays;
 
+/**
+ * This class implements the QuickSort algorithm to sort an array of integers.
+ */
 public class QuickSort {
+
+    /**
+     * The main method demonstrates the usage of the QuickSort algorithm on an array.
+     *
+     * @param args Command-line arguments (not used in this example).
+     */
     public static void main(String[] args) {
-        int[] arr = {5,4,3,2,1};
+        int[] arr = {5, 4, 3, 2, 1};
         quickSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
 
-    static void quickSort(int[] arr, int low, int high){
-        if(low >= high){
+    /**
+     * Sorts the given array using the QuickSort algorithm.
+     *
+     * @param arr  The array to be sorted.
+     * @param low  The starting index of the array or subarray.
+     * @param high The ending index of the array or subarray.
+     */
+    static void quickSort(int[] arr, int low, int high) {
+        if (low >= high) {
             return;
         }
 
-        int start = low;
-        int end = high;
-        int middle = start + (end - start) / 2;
+        int pivotIndex = partition(arr, low, high);
+
+        quickSort(arr, low, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, high);
+    }
+
+    /**
+     * Partitions the array and returns the index of the pivot element.
+     *
+     * @param arr  The array to be partitioned.
+     * @param low  The starting index of the array or subarray.
+     * @param high The ending index of the array or subarray.
+     * @return The index of the pivot element after partitioning.
+     */
+    static int partition(int[] arr, int low, int high) {
+        int middle = low + (high - low) / 2;
         int pivot = arr[middle];
 
-        while(start <= end){
-            while(arr[start] < pivot){
+        int start = low;
+        int end = high;
+
+        while (start <= end) {
+            while (arr[start] < pivot) {
                 start++;
             }
 
-            while(arr[end] > pivot){
+            while (arr[end] > pivot) {
                 end--;
             }
 
-            if(start <= end){
-                int temp = arr[start];
-                arr[start] = arr[end];
-                arr[end] = temp;
+            if (start <= end) {
+                swap(arr, start, end);
                 start++;
                 end--;
             }
-
-            // Now my pivot is at the correct index, please sort two halves now
-            quickSort(arr, low, end);
-            quickSort(arr, start, high);
         }
+
+        return start;
+    }
+
+    /**
+     * Swaps two elements in the array.
+     *
+     * @param arr The array containing the elements to be swapped.
+     * @param i   The index of the first element.
+     * @param j   The index of the second element.
+     */
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
