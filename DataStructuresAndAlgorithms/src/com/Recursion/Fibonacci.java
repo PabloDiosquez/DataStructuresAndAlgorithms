bab2afc53d9555c85f7047166cda8e4e20c71a10
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-21 23:30:40 -0300

    Add Fibo

---
+++ DataStructuresAndAlgorithms/src/com/Recursion/Fibonacci.java
@@ -0,0 +1,32 @@
+package com.Recursion;
+
+/**
+ * This class calculates Fibonacci numbers using recursion.
+ */
+public class Fibonacci {
+
+    /**
+     * The main method demonstrates the calculation of a Fibonacci number and prints the result.
+     *
+     * @param args Command-line arguments (not used in this example).
+     */
+    public static void main(String[] args) {
+        System.out.println(fibo(4));
+    }
+
+    /**
+     * Calculates the nth Fibonacci number using recursion.
+     *
+     * @param n The position of the Fibonacci number to be calculated.
+     * @return The nth Fibonacci number.
+     */
+    static int fibo(int n) {
+        // Base cases: 0 and 1 are the first two Fibonacci numbers.
+        if (n <= 1) {
+            return n;
+        }
+
+        // Recursive call to calculate the nth Fibonacci number.
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
