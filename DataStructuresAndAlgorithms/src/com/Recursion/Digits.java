7982b8d0179ab58b9b5f6622ad2cd600501db844
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-23 20:33:18 -0300

    Add Digits

--- DataStructuresAndAlgorithms/src/com/Recursion/Digits.java
+++ DataStructuresAndAlgorithms/src/com/Recursion/Digits.java
@@ -1,25 +1,57 @@
 package com.Recursion;
 
+/**
+ * The Digits class provides examples of recursive methods to calculate
+ * the sum and product of the digits of an integer.
+ */
 public class Digits {
+
+    /**
+     * The main method is the entry point of the program.
+     *
+     * @param args The command-line arguments provided to the program.
+     */
     public static void main(String[] args) {
+        // Example usage of the sumOfDigits and productOfDigits methods
         System.out.println(sumOfDigits(1234));
         System.out.println(productOfDigits(1234));
     }
 
-    static int sumOfDigits(int n){
-        if(n <= 9){
+    /**
+     * Recursively calculates the sum of the digits of the given integer.
+     *
+     * @param n The integer for which to calculate the sum of digits.
+     * @return The sum of the digits of the given integer.
+     */
+    static int sumOfDigits(int n) {
+        // Base case: if n is 0, the sum is 0
+        if (n == 0) {
             return n;
         }
 
-        int digit = n % 10;
-        return digit + sumOfDigits(n / 10);
+        // Extract the last digit of n
+        int lastDigit = n % 10;
+
+        // Recursive call to calculate the sum of digits for the remaining part of n
+        return lastDigit + sumOfDigits(n / 10);
     }
 
-    static int productOfDigits(int n){
-        if(n <= 9){
-            return n;
+    /**
+     * Recursively calculates the product of the digits of the given integer.
+     *
+     * @param n The integer for which to calculate the product of digits.
+     * @return The product of the digits of the given integer.
+     */
+    static int productOfDigits(int n) {
+        // Base case: if n is 0, the product is 1
+        if (n == 0) {
+            return 1;
         }
-        int digit = n % 10;
-        return productOfDigits(n / 10) * digit;
+
+        // Extract the last digit of n
+        int lastDigit = n % 10;
+
+        // Recursive call to calculate the product of digits for the remaining part of n
+        return productOfDigits(n / 10) * lastDigit;
     }
 }
