342780897f7d0129c4debe6ef06a980c339ec457
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-23 19:55:11 -0300

    Add NumbersRevisited

--- DataStructuresAndAlgorithms/src/com/Recursion/NumbersRevisited.java
+++ DataStructuresAndAlgorithms/src/com/Recursion/NumbersRevisited.java
@@ -12,7 +12,7 @@ public class NumbersRevisited {
      * @param args The command-line arguments provided to the program.
      */
     public static void main(String[] args) {
-        print(5);
+        printBoth(5);
     }
 
     /**
@@ -31,6 +31,47 @@ public class NumbersRevisited {
 
         // Recursive call to print the next number (n-1)
         print(n - 1);
+    }
+
+    /**
+     * Recursively prints the numbers from 1 to the given integer.
+     * This version of the method prints the numbers before making the recursive call.
+     *
+     * @param n The integer up to which numbers should be printed.
+     */
+    static void printRev(int n) {
+        // Base case: if n is 0, terminate the recursion
+        if (n == 0) {
+            return;
+        }
+
+        // Recursive call to print the numbers from 1 to n-1
+        printRev(n - 1);
+
+        // Print the current value of n
+        System.out.println(n);
+    }
+
+    /**
+     * Recursively prints the numbers from the given integer down to 1
+     * and then back up to the original integer.
+     *
+     * @param n The integer from which to start and end printing.
+     */
+    static void printBoth(int n) {
+        // Base case: if n is 0, terminate the recursion
+        if (n == 0) {
+            return;
+        }
+
+        // Print the current value of n before making the recursive call
+        System.out.println(n);
+
+        // Recursive call to print the numbers from 1 to n-1
+        printBoth(n - 1);
+
+        // Print the current value of n after the recursive call
+        System.out.println(n);
     }
 }
 
