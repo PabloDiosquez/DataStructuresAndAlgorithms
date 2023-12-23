6dd6c1c0f7954fa7a533c6b72aee64c70a9acf7a
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-23 20:06:37 -0300

    Add Factorial

--- DataStructuresAndAlgorithms/src/com/Recursion/Factorial.java
+++ DataStructuresAndAlgorithms/src/com/Recursion/Factorial.java
@@ -1,14 +1,34 @@
 package com.Recursion;
 
+/**
+ * The Factorial class provides a simple example of recursive
+ * calculation of factorial using a factorial method.
+ */
 public class Factorial {
+
+    /**
+     * The main method is the entry point of the program.
+     *
+     * @param args The command-line arguments provided to the program.
+     */
     public static void main(String[] args) {
+        // Example usage of the fact method to calculate and print the factorial of 5
         System.out.println(fact(5));
     }
 
-    static int fact(int n){
-        if(n == 0){
+    /**
+     * Recursively calculates the factorial of the given integer.
+     *
+     * @param n The integer for which to calculate the factorial.
+     * @return The factorial of the given integer.
+     */
+    static int fact(int n) {
+        // Base case: if n is 0, the factorial is 1
+        if (n == 0) {
             return 1;
         }
-        return fact(n-1) * n;
+
+        // Recursive call to calculate the factorial of n-1, multiplied by n
+        return fact(n - 1) * n;
     }
 }
