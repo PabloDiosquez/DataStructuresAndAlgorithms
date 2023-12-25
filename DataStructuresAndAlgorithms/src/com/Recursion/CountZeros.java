87d0a887f75a9cc3e8af7dcfc0b58199bd1b6eef
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-25 16:12:10 -0300

    CountZeros

--- DataStructuresAndAlgorithms/src/com/Recursion/CountZeros.java
+++ DataStructuresAndAlgorithms/src/com/Recursion/CountZeros.java
@@ -2,28 +2,31 @@ package com.Recursion;
 
 public class CountZeros {
     public static void main(String[] args) {
-        System.out.println(countZeros(100101));
+        System.out.println(countZerosV1(30204));
+        System.out.println(countZerosV2(30204));
     }
 
-    static int countZeros(int number){
+    static int countZerosV1(int number){
+        return _countZerosV1(number, 0);
+    }
+    private static int _countZerosV1(int number, int count){
+        if(number == 0){
+            return count;
+        }
+
+        if(number % 10 == 0){
+            return _countZerosV1(number / 10, count + 1);
+        }
+        return _countZerosV1(number / 10, count);
+    }
+    static int countZerosV2(int number){
         if(number == 0){
             return 0;
         }
-        return countZeros(number / 10) + zero(number % 10);
+        return countZerosV2(number / 10) + zero(number % 10);
     }
-
     private static int zero(int digit){
         if(digit == 0) return 1;
         return 0;
     }
-
-
-
-
-
-
-
-
-
-
 }
