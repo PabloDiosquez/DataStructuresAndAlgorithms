1514fa9f2d27d0700fef1a1371c0b6ae18cf58ab
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-26 11:46:13 -0300

    Added Triangles

--- DataStructuresAndAlgorithms/src/com/Recursion/Triangles.java
+++ DataStructuresAndAlgorithms/src/com/Recursion/Triangles.java
@@ -3,9 +3,11 @@ package com.Recursion;
 public class Triangles {
     public static void main(String[] args) {
         triangle(4);
+        System.out.println();
+        triangleRev(4);
     }
 
-    static void triangle(int n){
+    static void triangleRev(int n){
         if(n == 0){
             return;
         }
@@ -13,7 +15,17 @@ public class Triangles {
         paint('*', n);
 
         System.out.println();
+        triangleRev(n-1);
+    }
+
+    static void triangle(int n){
+        if(n == 0){
+            return;
+        }
+
         triangle(n-1);
+        paint('*', n);
+        System.out.println();
     }
 
     static void paint(char c, int n){
