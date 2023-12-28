664e3c9e8bbb7e7a35002523ead36c9a8f0783b0
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-28 14:11:50 -0300

    Added Subset

--- DataStructuresAndAlgorithms/src/com/Recursion/Subset.java
+++ DataStructuresAndAlgorithms/src/com/Recursion/Subset.java
@@ -5,20 +5,65 @@ import java.util.ArrayList;
 public class Subset {
     public static void main(String[] args) {
         String s = "abc";
-        subsets(s);
+        //subsets(s);
+        System.out.println(substrings(s, "", new ArrayList<String>(), new ArrayList<String>()));
     }
 
-    static void subsets(String up){
-        subsets("", up);
+    /**
+     * Generates and prints all possible subsets of the given string.
+     *
+     * @param input The input string for which subsets are to be generated.
+     */
+    public static void subsets(String input) {
+        subsets("", input);
     }
-    private static void subsets(String p, String up){
-        if(up.isEmpty()){
-            System.out.println(p);
+
+    /**
+     * Recursively generates and prints subsets of the given string.
+     *
+     * @param currentSubset The current subset being generated.
+     * @param remainingChars The remaining characters in the input string.
+     */
+    private static void subsets(String currentSubset, String remainingChars) {
+        if (remainingChars.isEmpty()) {
+            // Base case: Print the current subset when no characters are left.
+            System.out.println(currentSubset);
             return;
         }
 
+        // Include the first character and recurse
+        char firstChar = remainingChars.charAt(0);
+        subsets(currentSubset + firstChar, remainingChars.substring(1));
+
+        // Exclude the first character and recurse
+        subsets(currentSubset, remainingChars.substring(1));
+    }
+
+    // ##############################################
+
+    static ArrayList<String> substrings(String up, String p, ArrayList<String> first, ArrayList<String> second){
+        if(up.isEmpty()){
+            first.addAll(second);
+            return first;
+        }
+
         char ch = up.charAt(0);
-        subsets(ch + p, up.substring(1));
-        subsets(p, up.substring(1));
+        second.add(ch + p);
+        first.add(p);
+        return substrings(up.substring(1), ch+p, first, second);
+
+    }
+    private static void addToEveryone(char c, ArrayList<String> strings){
+        _addToEveryone(c, new ArrayList<String>(), strings, 0);
+    }
+
+    private static ArrayList<String> _addToEveryone(char c, ArrayList<String> actual, ArrayList<String> old, int index){
+        if(index == old.size()){
+            return actual;
+        }
+
+        String s = c + old.get(index);
+        actual.add(s);
+        return _addToEveryone(c, actual, old, index+1);
     }
 }
