236591f554f6f6ad3dc908acdf5f4f656f812ad1
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-01-05 22:52:00 -0300

    Added Counting Sort

--- DataStructuresAndAlgorithms/src/com/Sorting/CountingSort/CountingSort.java
+++ DataStructuresAndAlgorithms/src/com/Sorting/CountingSort/CountingSort.java
@@ -2,30 +2,49 @@ package com.Sorting.CountingSort;
 
 import java.util.Arrays;
 
+/**
+ * This class implements the Counting Sort algorithm for integer arrays.
+ */
 public class CountingSort {
+
+    /**
+     * The main method demonstrates the usage of the Counting Sort algorithm.
+     * It initializes an array, sorts it using the countSort method, and prints the result.
+     *
+     * @param args The command-line arguments (not used in this example).
+     */
     public static void main(String[] args) {
-        int[] arr = new int[] {3,4,1,3,2,5,2,3};
+        int[] arr = new int[] {3, 4, 1, 3, 2, 5, 2, 3};
+
+        // Using the countSort method to sort the array
         countSort(arr);
-        System.out.println(Arrays.toString(arr));
+
+        // Printing the sorted array
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
     }
 
-    public static void countSort(int[] arr){
-        if(arr == null || arr.length <= 1){
+    /**
+     * Sorts an integer array using the Counting Sort algorithm.
+     * Modifies the input array in-place.
+     *
+     * @param arr The integer array to be sorted.
+     */
+    public static void countSort(int[] arr) {
+        if (arr == null || arr.length <= 1) {
             return;
         }
 
-        int max = findMax(arr);
-
-        int[] countArr = new int[max+1];
+        int max = Arrays.stream(arr).max().getAsInt();
+        int[] countArr = new int[max + 1];
 
         // Frequency array
-        for(int num: arr){
+        for (int num : arr) {
             countArr[num]++;
         }
 
         int index = 0;
-        for (int i = 0; i < max; i++) {
-            while(countArr[i] > 0){
+        for (int i = 0; i <= max; i++) {
+            while (countArr[i] > 0) {
                 arr[index] = i;
                 index++;
                 countArr[i]--;
@@ -33,18 +52,25 @@ public class CountingSort {
         }
     }
 
-    public static int[] countingSort(int[] arr){
+    /**
+     * Sorts an integer array using the Counting Sort algorithm.
+     * Returns a new sorted array without modifying the input array.
+     *
+     * @param arr The integer array to be sorted.
+     * @return A new array containing the sorted elements.
+     */
+    public static int[] countingSort(int[] arr) {
         int max_value = findMax(arr);
-        int[] count = new int[max_value+1];
-        int[] sums  = new int[max_value+1];
-        int[] ans   = new int[arr.length];
+        int[] count = new int[max_value + 1];
+        int[] sums = new int[max_value + 1];
+        int[] ans = new int[arr.length];
 
         for (int i = 0; i < arr.length; i++) {
             count[arr[i]]++;
         }
 
-        for (int i = 1; i < max_value+1; i++) {
-            sums[i] = sums[i-1] + count[i-1];
+        for (int i = 1; i <= max_value; i++) {
+            sums[i] = sums[i - 1] + count[i - 1];
         }
 
         for (int i = 0; i < arr.length; i++) {
@@ -54,10 +80,16 @@ public class CountingSort {
         return ans;
     }
 
+    /**
+     * Finds the maximum value in an integer array.
+     *
+     * @param arr The integer array.
+     * @return The maximum value in the array.
+     */
     private static int findMax(int[] arr) {
         int max_value = arr[0];
-        for (int num : arr){
-            if(num > max_value){
+        for (int num : arr) {
+            if (num > max_value) {
                 max_value = num;
             }
         }
