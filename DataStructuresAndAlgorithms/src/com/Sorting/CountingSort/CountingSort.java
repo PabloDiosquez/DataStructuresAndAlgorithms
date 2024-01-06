8ab47ca33d08eaf2b2bb0dffb27ed131925ffa9e
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-01-05 22:44:48 -0300

    Added Counting Sort

--- DataStructuresAndAlgorithms/src/com/Sorting/CountingSort/CountingSort.java
+++ DataStructuresAndAlgorithms/src/com/Sorting/CountingSort/CountingSort.java
@@ -5,7 +5,32 @@ import java.util.Arrays;
 public class CountingSort {
     public static void main(String[] args) {
         int[] arr = new int[] {3,4,1,3,2,5,2,3};
-        System.out.println(Arrays.toString(countingSort(arr)));
+        countSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void countSort(int[] arr){
+        if(arr == null || arr.length <= 1){
+            return;
+        }
+
+        int max = findMax(arr);
+
+        int[] countArr = new int[max+1];
+
+        // Frequency array
+        for(int num: arr){
+            countArr[num]++;
+        }
+
+        int index = 0;
+        for (int i = 0; i < max; i++) {
+            while(countArr[i] > 0){
+                arr[index] = i;
+                index++;
+                countArr[i]--;
+            }
+        }
     }
 
     public static int[] countingSort(int[] arr){
@@ -15,7 +40,7 @@ public class CountingSort {
         int[] ans   = new int[arr.length];
 
         for (int i = 0; i < arr.length; i++) {
-            count[arr[i]] += 1;
+            count[arr[i]]++;
         }
 
         for (int i = 1; i < max_value+1; i++) {
@@ -26,15 +51,14 @@ public class CountingSort {
             ans[sums[arr[i]]] = arr[i];
             sums[arr[i]]++;
         }
-
         return ans;
     }
 
     private static int findMax(int[] arr) {
         int max_value = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if(arr[i] > max_value){
-                max_value = arr[i];
+        for (int num : arr){
+            if(num > max_value){
+                max_value = num;
             }
         }
         return max_value;
