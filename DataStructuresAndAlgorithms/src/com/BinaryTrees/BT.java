cd318448c9cff6dcc03419b90f24864999417c82
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-01-08 17:27:30 -0300

    Added BT

--- DataStructuresAndAlgorithms/src/com/BinaryTrees/BT.java
+++ DataStructuresAndAlgorithms/src/com/BinaryTrees/BT.java
@@ -2,29 +2,54 @@ package com.BinaryTrees;
 
 import java.util.Scanner;
 
+/**
+ * Binary Tree class with methods to populate, display, and pretty display the tree.
+ */
 public class BT {
+
+    /**
+     * Main method to execute the binary tree operations.
+     *
+     * @param args Command line arguments (not used).
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         BT tree = new BT();
         tree.populate(sc);
+        // Uncomment the next line to display the tree in a simple format
         // tree.display();
         tree.prettyDisplay();
     }
 
     private Node root;
 
-    public BT(){
+    /**
+     * Default constructor to initialize the binary tree.
+     */
+    public BT() {
     }
 
-    public void populate(Scanner scanner){
+    /**
+     * Populates the binary tree with user input values.
+     *
+     * @param scanner Scanner object to read user input.
+     */
+    public void populate(Scanner scanner) {
         System.out.println("Enter the root node:");
         this.root = new Node(scanner.nextInt());
         populate(scanner, root);
     }
+
+    /**
+     * Recursively populates the binary tree based on user input.
+     *
+     * @param scanner Scanner object to read user input.
+     * @param node    Current node to populate.
+     */
     private void populate(Scanner scanner, Node node) {
         System.out.println("Do you want to enter left of " + node.value + "?");
         boolean left = scanner.nextBoolean();
-        if(left){
+        if (left) {
             System.out.println("Enter the value of the left of " + node.value + "?");
             int value = scanner.nextInt();
             node.left = new Node(value);
@@ -33,7 +58,7 @@ public class BT {
 
         System.out.println("Do you want to enter right of " + node.value + "?");
         boolean right = scanner.nextBoolean();
-        if(right){
+        if (right) {
             System.out.println("Enter the value of the right of " + node.value + "?");
             int value = scanner.nextInt();
             node.right = new Node(value);
@@ -41,11 +66,21 @@ public class BT {
         }
     }
 
-    public void display(){
+    /**
+     * Displays the binary tree in a simple format.
+     */
+    public void display() {
         display(this.root, "");
     }
+
+    /**
+     * Recursively displays the binary tree.
+     *
+     * @param node   Current node to display.
+     * @param indent Current indentation for formatting.
+     */
     private void display(Node node, String indent) {
-        if(node == null){
+        if (node == null) {
             return;
         }
         System.out.println(indent + node.value);
@@ -53,48 +88,82 @@ public class BT {
         display(node.right, indent + "\t");
     }
 
-    public void prettyDisplay(){
+    /**
+     * Pretty displays the binary tree with proper indentation.
+     */
+    public void prettyDisplay() {
         prettyDisplay(this.root, 0);
     }
 
+    /**
+     * Recursively pretty displays the binary tree with proper indentation.
+     *
+     * @param node  Current node to display.
+     * @param level Current level of the tree.
+     */
     private void prettyDisplay(Node node, int level) {
-        if(node == null){
+        if (node == null) {
             return;
         }
 
-        prettyDisplay(node.right, level+1);
+        prettyDisplay(node.right, level + 1);
 
-        if(level != 0){
-            for (int i = 0; i < level-1; i++) {
-                System.out.println("|\t\t");
+        if (level != 0) {
+            for (int i = 0; i < level - 1; i++) {
+                System.out.print("|\t\t");
             }
             System.out.println("|------> " + node.value);
         } else {
             System.out.println(node.value);
         }
 
-        prettyDisplay(node.left, level+1);
+        prettyDisplay(node.left, level + 1);
     }
 
-    private class Node{
+    /**
+     * Node class representing a node in the binary tree.
+     */
+    private class Node {
         private int value;
         private Node left;
         private Node right;
 
-        public Node(int value){
+        /**
+         * Constructor to create a node with a given value.
+         *
+         * @param value Value of the node.
+         */
+        public Node(int value) {
             this.value = value;
         }
 
-        public Node(int value, Node left, Node right){
+        /**
+         * Constructor to create a node with a given value, left, and right child nodes.
+         *
+         * @param value Value of the node.
+         * @param left  Left child node.
+         * @param right Right child node.
+         */
+        public Node(int value, Node left, Node right) {
             this(value);
             this.left = left;
             this.right = right;
         }
 
-        public Node(Node node){
+        /**
+         * Copy constructor to create a node based on another node.
+         *
+         * @param node Node to copy.
+         */
+        public Node(Node node) {
             this(node.value, node.left, node.right);
         }
 
+        /**
+         * String representation of the node.
+         *
+         * @return String representation of the node.
+         */
         @Override
         public String toString() {
             return "Node value: %s".formatted(this.value);
