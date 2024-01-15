15467b52753bc3d79d180ef6b4e681ab11221d4a
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-01-15 18:37:20 -0300

    Added BS_2D

--- DataStructuresAndAlgorithms/src/com/BinarySearch/BS_2D.java
+++ DataStructuresAndAlgorithms/src/com/BinarySearch/BS_2D.java
@@ -47,19 +47,54 @@ public class BS_2D {
         return new int[]{-1, -1};
     }
 
-    static int[] binSearch(int[][] sortedArr, int target){
-        return null;
+    static int[] binSearch(int[][] matrix, int target){
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        if(rows == 1){
+            return binarySearch(matrix, 0, 0, cols-1, target);
+        }
+
+        int rStart = 0;
+        int rEnd = rows-1;
+        int cMid = cols / 2;
+
+        while (rStart < rEnd-1){
+            int middle = rStart + (rEnd - rStart) / 2;
+
+            if(matrix[middle][cMid] == target){
+                return new int[] {middle, cMid};
+            }
+            if(matrix[middle][cMid] < target){
+                rStart = middle;
+            } else {
+                rEnd = middle;
+            }
+        }
+
+        // Now we have 2 rows
+        if(matrix[rStart][cMid] == target){
+            return new int[]{rStart, cMid};
+        }
+        if(matrix[rStart+1][cMid] == target){
+            return new int[]{rStart+1, cMid};
+        }
+
+        // Search in 1st half
+        if(target <= matrix[rStart][cMid]){
+            return binarySearch(matrix, rStart, 0, cMid-1, target);
+        }
+
     }
 
-    private static int[] binarySearch(int[] arr, int row, int colStart, int colEnd, int target){
+    private static int[] binarySearch(int[][] arr, int row, int colStart, int colEnd, int target){
         while(colStart <= colEnd){
-            int middle = (colStart + colEnd) / 2;
+            int middle = colStart + (colEnd - colStart) / 2;
 
-            if(arr[middle] == target){
+            if(arr[row][middle] == target){
                 return new int[] {row, middle};
             }
-
-            if(arr[middle] < target){
+            if(arr[row][middle] < target){
                 colStart = middle+1;
             } else {
                 colEnd = middle-1;
