b5ba26cc8bbc16482abe0f407d62b42c84b0ef4b
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-01-16 11:46:50 -0300

    Added CustomStack

--- DataStructuresAndAlgorithms/src/com/Stack/StackException.java
+++ DataStructuresAndAlgorithms/src/com/Stack/StackException.java
@@ -1,7 +1,17 @@
 package com.Stack;
 
-public class StackException extends Exception{
-    public StackException(String message){
+/**
+ * StackException is a custom exception class used to indicate exceptional conditions specific to a stack.
+ * It extends the standard Exception class.
+ */
+public class StackException extends Exception {
+
+    /**
+     * Constructs a new StackException with the specified detail message.
+     *
+     * @param message The detail message, which is saved for later retrieval by the getMessage() method.
+     */
+    public StackException(String message) {
         super(message);
     }
 }
