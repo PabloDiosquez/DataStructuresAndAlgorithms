19452793b9560e769635d7f7c1809a5f719c26f3
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-01-16 11:29:34 -0300

    Added CustomStack

--- DataStructuresAndAlgorithms/src/com/Stack/CustomStack.java
+++ DataStructuresAndAlgorithms/src/com/Stack/CustomStack.java
@@ -24,6 +24,23 @@ public class CustomStack {
         return false;
     }
 
+    public int pop() throws Exception{
+        if(this.isEmpty()){
+            throw new Exception("Cannot pop from an empty stack!");
+        }
+        //int removed = this.data[pointer];
+        //pointer--;
+        //return removed;
+        return data[pointer--];
+    }
+
+    public int peek() throws Exception{
+        if(this.isEmpty()){
+            throw new Exception("Cannot peek from an empty stack!");
+        }
+        return this.data[pointer];
+    }
+
     private boolean IsNotFull() {
         return this.pointer < this.data.length-1;
     }
