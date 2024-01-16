e349aca9e76b248586a1f04956352a478d215986
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-01-16 11:46:01 -0300

    Added CustomStack

--- DataStructuresAndAlgorithms/src/com/Stack/CustomStack.java
+++ DataStructuresAndAlgorithms/src/com/Stack/CustomStack.java
@@ -1,21 +1,53 @@
 package com.Stack;
 
+import com.Stack.StackException;
+
+/**
+ * CustomStack is a simple implementation of a stack using an array.
+ * It provides basic stack operations such as push, pop, and peek.
+ */
 public class CustomStack {
+
+    /**
+     * The underlying array to store stack elements.
+     */
     protected int[] data;
+
+    /**
+     * The default size of the stack if no size is specified during instantiation.
+     */
     private static final int DEFAULT_SIZE = 10;
 
+    /**
+     * The index indicating the top of the stack.
+     */
     private int pointer;
 
-    public CustomStack(){
+    /**
+     * Constructs a stack with the default size.
+     */
+    public CustomStack() {
         this(DEFAULT_SIZE);
     }
-    public CustomStack(int size){
+
+    /**
+     * Constructs a stack with the specified size.
+     *
+     * @param size The size of the stack.
+     */
+    public CustomStack(int size) {
         this.data = new int[size];
         this.pointer = -1;
     }
 
-    public boolean push(int item){
-        if(this.IsNotFull()){
+    /**
+     * Pushes an item onto the top of the stack.
+     *
+     * @param item The item to be pushed onto the stack.
+     * @return True if the push operation is successful, false if the stack is full.
+     */
+    public boolean push(int item) {
+        if (this.isNotFull()) {
             this.pointer++;
             this.data[pointer] = item;
             return true;
@@ -24,28 +56,47 @@ public class CustomStack {
         return false;
     }
 
-    public int pop() throws Exception{
-        if(this.isEmpty()){
-            throw new Exception("Cannot pop from an empty stack!");
+    /**
+     * Pops the top element from the stack.
+     *
+     * @return The popped element.
+     * @throws StackException If the stack is empty.
+     */
+    public int pop() throws StackException {
+        if (this.isEmpty()) {
+            throw new StackException("Cannot pop from an empty stack!");
         }
-        //int removed = this.data[pointer];
-        //pointer--;
-        //return removed;
         return data[pointer--];
     }
 
-    public int peek() throws Exception{
-        if(this.isEmpty()){
-            throw new Exception("Cannot peek from an empty stack!");
+    /**
+     * Peeks at the top element of the stack without removing it.
+     *
+     * @return The element at the top of the stack.
+     * @throws StackException If the stack is empty.
+     */
+    public int peek() throws StackException {
+        if (this.isEmpty()) {
+            throw new StackException("Cannot peek from an empty stack!");
         }
         return this.data[pointer];
     }
 
-    private boolean IsNotFull() {
-        return this.pointer < this.data.length-1;
+    /**
+     * Checks if the stack is not full.
+     *
+     * @return True if the stack is not full, false otherwise.
+     */
+    private boolean isNotFull() {
+        return this.pointer < this.data.length - 1;
     }
 
-    private boolean isEmpty(){
+    /**
+     * Checks if the stack is empty.
+     *
+     * @return True if the stack is empty, false otherwise.
+     */
+    private boolean isEmpty() {
         return this.pointer == -1;
     }
 }
---
+++ DataStructuresAndAlgorithms/src/com/Stack/StackMain.java
@@ -0,0 +1,7 @@
+package com.Stack;
+
+public class StackMain {
+    public static void main(String[] args) {
+
+    }
+}
