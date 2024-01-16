9f48de8d6d20329993a287f5bc1d8f843df1165f
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-01-16 10:42:00 -0300

    Added InBuiltExamples

--- DataStructuresAndAlgorithms/src/com/Stack/InBuiltExamples.java
+++ DataStructuresAndAlgorithms/src/com/Stack/InBuiltExamples.java
@@ -1,5 +1,7 @@
 package com.Stack;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class InBuiltExamples {
@@ -12,8 +14,25 @@ public class InBuiltExamples {
         stack.push(91);
         stack.push(54);
 
+        System.out.println("Stack:");
         System.out.println(stack.pop());
         System.out.println(stack.pop());
         System.out.println(stack.pop());
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(3);
+        queue.add(12);
+        queue.add(5);
+        queue.add(19);
+        queue.add(1);
+
+        System.out.println("\nQueue:");
+        System.out.println(queue.peek());
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+
+
     }
 }
