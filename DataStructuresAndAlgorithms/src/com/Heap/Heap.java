89b8c92ed4de966016183db0a95455f038b8f4cd
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-01-18 17:04:07 -0300

    Added Heap

--- DataStructuresAndAlgorithms/src/com/Heap/Heap.java
+++ DataStructuresAndAlgorithms/src/com/Heap/Heap.java
@@ -2,77 +2,135 @@ package com.Heap;
 
 import java.util.ArrayList;
 
+/**
+ * A generic heap implementation using an ArrayList.
+ *
+ * @param <T> the type of elements stored in the heap, must extend Comparable
+ */
 public class Heap<T extends Comparable<T>> {
+
+    /**
+     * The internal ArrayList to store elements in the heap.
+     */
     private ArrayList<T> list;
 
-    public Heap(){
+    /**
+     * Constructs an empty heap.
+     */
+    public Heap() {
         this.list = new ArrayList<>();
     }
-    private void swap(int first, int second){
+
+    /**
+     * Swaps two elements in the internal ArrayList.
+     *
+     * @param first  the index of the first element
+     * @param second the index of the second element
+     */
+    private void swap(int first, int second) {
         T temp = this.list.get(first);
         this.list.set(first, this.list.get(second));
         this.list.set(second, temp);
     }
 
-    public int parent(int index){
-        return (index-1) / 2;
+    /**
+     * Returns the index of the parent node for the given index.
+     *
+     * @param index the index of the node
+     * @return the index of the parent node
+     */
+    public int parent(int index) {
+        return (index - 1) / 2;
     }
 
-    public int left(int index){
+    /**
+     * Returns the index of the left child node for the given index.
+     *
+     * @param index the index of the node
+     * @return the index of the left child node
+     */
+    public int left(int index) {
         return 2 * index + 1;
     }
 
-    public int right(int index){
+    /**
+     * Returns the index of the right child node for the given index.
+     *
+     * @param index the index of the node
+     * @return the index of the right child node
+     */
+    public int right(int index) {
         return 2 * index + 2;
     }
 
-    public void insert(T value){
+    /**
+     * Inserts a value into the heap and maintains the heap property.
+     *
+     * @param value the value to be inserted
+     */
+    public void insert(T value) {
         this.list.add(value);
-        unheap(this.list.size()-1);
+        upheap(this.list.size() - 1);
     }
 
-    private void unheap(int index) {
-        if(index == 0){
+    /**
+     * Helper method to move the element up the heap to maintain the heap property.
+     *
+     * @param index the index of the element to be moved up
+     */
+    private void upheap(int index) {
+        if (index == 0) {
             return;
         }
         int p = this.parent(index);
-        if(this.list.get(index).compareTo(this.list.get(p)) < 0){
+        if (this.list.get(index).compareTo(this.list.get(p)) < 0) {
             swap(index, p);
-            unheap(p);
+            upheap(p);
         }
     }
 
-    public T remove() throws Exception{
-        if(this.list.isEmpty()){
+    /**
+     * Removes and returns the root element of the heap.
+     *
+     * @return the root element of the heap
+     * @throws Exception if removing from an empty heap
+     */
+    public T remove() throws Exception {
+        if (this.list.isEmpty()) {
             throw new Exception("Removing from an empty heap!");
         }
 
         T removed = this.list.get(0);
 
-        T last = this.list.get(this.list.size()-1);
-        if(!this.list.isEmpty()){
+        T last = this.list.get(this.list.size() - 1);
+        if (!this.list.isEmpty()) {
             this.list.set(0, last);
             this.downheap(0);
         }
         return removed;
     }
 
+    /**
+     * Helper method to move the element down the heap to maintain the heap property.
+     *
+     * @param index the index of the element to be moved down
+     */
     private void downheap(int index) {
         int min = index;
         int left = this.left(index);
         int right = this.right(index);
 
-        if(left < this.list.size() &&
-                this.list.get(min).compareTo(this.list.get(left)) > 0){
+        if (left < this.list.size() &&
+                this.list.get(min).compareTo(this.list.get(left)) > 0) {
             min = left;
         }
 
-        if(right < this.list.size() &&
-                this.list.get(min).compareTo(this.list.get(right)) > 0){
+        if (right < this.list.size() &&
+                this.list.get(min).compareTo(this.list.get(right)) > 0) {
             min = right;
         }
 
-        if(min != index){
+        if (min != index) {
             swap(min, index);
             downheap(min);
         }
