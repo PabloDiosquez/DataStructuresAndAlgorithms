31e40d92ece13e5b2526b9adca36830d452ad0e1
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-01-18 16:37:32 -0300

    Added Heap

--- DataStructuresAndAlgorithms/src/com/Heap/Heap.java
+++ DataStructuresAndAlgorithms/src/com/Heap/Heap.java
@@ -13,4 +13,32 @@ public class Heap<T extends Comparable<T>> {
         this.list.set(first, this.list.get(second));
         this.list.set(second, temp);
     }
+
+    public int parent(int index){
+        return (index-1) / 2;
+    }
+
+    public int left(int index){
+        return 2 * index + 1;
+    }
+
+    public int right(int index){
+        return 2 * index + 2;
+    }
+
+    public void insert(T value){
+        this.list.add(value);
+        unheap(this.list.size()-1);
+    }
+
+    private void unheap(int index) {
+        if(index == 0){
+            return;
+        }
+        int p = this.parent(index);
+        if(this.list.get(index).compareTo(this.list.get(p)) < 0){
+            swap(index, p);
+            unheap(p);
+        }
+    }
 }
