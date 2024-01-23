b2ae772d335644e279b1365ef5526fce0f6691ab
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-01-22 21:45:07 -0300

    Added Hashmaps.java

--- DataStructuresAndAlgorithms/src/com/HashMap/Hashmaps.java
+++ DataStructuresAndAlgorithms/src/com/HashMap/Hashmaps.java
@@ -1,11 +1,42 @@
 package com.HashMap;
 
+/**
+ * The Hashmaps class demonstrates the usage of HashMaps in Java.
+ */
 public class Hashmaps {
+
+    /**
+     * The main method is the entry point of the program.
+     *
+     * @param args Command-line arguments (not used in this example).
+     */
     public static void main(String[] args) {
+        // Step 1: Initialize a string
         String name = "Kunal";
 
-        int code = name.hashCode();
+        // Step 2: Generate the hash code for the string
+        int hashCode = generateHashCode(name);
 
-        System.out.println(code);
+        // Step 3: Print the generated hash code
+        printHashCode(hashCode);
+    }
+
+    /**
+     * Generates the hash code for a given string.
+     *
+     * @param str The input string for which the hash code is to be generated.
+     * @return The hash code of the input string.
+     */
+    private static int generateHashCode(String str) {
+        return str.hashCode();
+    }
+
+    /**
+     * Prints the hash code to the console.
+     *
+     * @param hashCode The hash code to be printed.
+     */
+    private static void printHashCode(int hashCode) {
+        System.out.println("Generated Hash Code: " + hashCode);
     }
 }
