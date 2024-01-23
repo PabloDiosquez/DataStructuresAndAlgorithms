a90f8a7761c813e183808d38f6683046c66e32bf
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-01-22 21:42:14 -0300

    Added Hashmaps.java

---
+++ DataStructuresAndAlgorithms/src/com/HashMap/Hashmaps.java
@@ -0,0 +1,11 @@
+package com.HashMap;
+
+public class Hashmaps {
+    public static void main(String[] args) {
+        String name = "Kunal";
+
+        int code = name.hashCode();
+
+        System.out.println(code);
+    }
+}
