c7099cf11cd9fbfdb441c96d1aca07c1abb35616
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-01-25 17:43:02 -0300

    Added Packages

---
+++ DataStructuresAndAlgorithms/src/com/OOP/Packages/B/Greetings.java
@@ -0,0 +1,7 @@
+package com.OOP.Packages.B;
+
+public class Greetings {
+    public static void main(String[] args) {
+        System.out.println("Greetings from B!!!");
+    }
+}
---
+++ DataStructuresAndAlgorithms/src/com/OOP/Packages/Greetings.java
@@ -0,0 +1,7 @@
+package com.OOP.Packages;
+
+public class Greetings {
+    public static void main(String[] args) {
+        System.out.println("Greetings!!!");
+    }
+}
