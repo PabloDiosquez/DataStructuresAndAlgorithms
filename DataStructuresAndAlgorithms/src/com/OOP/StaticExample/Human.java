bd75e6c66a2cb530fe008ba1a4cf5b2f0e87a131
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-01-25 18:27:42 -0300

    Added StaticExample

--- DataStructuresAndAlgorithms/src/com/OOP/StaticExample/Human.java
+++ DataStructuresAndAlgorithms/src/com/OOP/StaticExample/Human.java
@@ -1,14 +1,46 @@
 package com.OOP.StaticExample;
 
+/**
+ * The {@code Human} class represents a basic human entity with attributes
+ * such as name, age, and salary.
+ */
 public class Human {
+
+    /**
+     * The name of the human.
+     */
     String name;
+
+    /**
+     * The age of the human.
+     */
     int age;
+
+    /**
+     * The salary of the human.
+     */
     float salary;
+
+    /**
+     * The total population of humans (static variable shared among all instances).
+     */
     static int population = 7000000;
 
+    /**
+     * Default constructor for creating a {@code Human} object with default values.
+     * The default name is "Default human", age is 0, and salary is 0.0F.
+     */
     public Human() {
         this("Default human", 0, 0.0F);
     }
+
+    /**
+     * Parameterized constructor for creating a {@code Human} object with specified values.
+     *
+     * @param name    The name of the human.
+     * @param age     The age of the human.
+     * @param salary  The salary of the human.
+     */
     public Human(String name, int age, float salary) {
         this.name = name;
         this.age = age;
--- DataStructuresAndAlgorithms/src/com/OOP/StaticExample/Main.java
+++ DataStructuresAndAlgorithms/src/com/OOP/StaticExample/Main.java
@@ -1,11 +1,24 @@
 package com.OOP.StaticExample;
 
+/**
+ * The {@code Main} class contains the main method to demonstrate the usage of the {@code Human} class.
+ */
 public class Main {
+
+    /**
+     * The main method that serves as the entry point of the program.
+     *
+     * @param args The command-line arguments (not used in this program).
+     */
     public static void main(String[] args) {
-        Human human1 = new Human();
-        Human human2 = new Human("Kunal Kushwaha", 25, 10100F);
+        // Display the initial population of humans
+        System.out.println("Initial Human Population: " + Human.population);
 
-        System.out.println(Human.population);
+        // Create two Human objects using different constructors
+        Human human1 = new Human(); // Using the default constructor
+        Human human2 = new Human("Kunal Kushwaha", 25, 10100F); // Using the parameterized constructor
 
+        // Display the updated population of humans after creating instances
+        System.out.println("Updated Human Population: " + Human.population);
     }
 }
