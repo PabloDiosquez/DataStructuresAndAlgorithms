fc0ab7ab8ccf20bc8b989f80fc92841ec5d82605
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-01-29 16:08:23 -0300

    Added StaticInit

--- DataStructuresAndAlgorithms/src/com/OOP/StaticExample/StaticInit.java
+++ DataStructuresAndAlgorithms/src/com/OOP/StaticExample/StaticInit.java
@@ -1,28 +1,48 @@
 package com.OOP.StaticExample;
 
+/**
+ * The {@code StaticInit} class demonstrates the usage of static initialization blocks in Java.
+ * It initializes static variables 'a' and 'b' in a static block, ensuring they are computed
+ * and assigned only once when the first object is created.
+ */
 public class StaticInit {
+
+    /** Represents the first static variable. */
     static int a;
+
+    /** Represents the second static variable, calculated based on 'a'. */
     static int b;
 
-    //Static block
-
-    // Will only run once, when the first obj is created.
-    static{
+    /**
+     * This static block is executed only once, when the class is loaded into memory.
+     * It initializes static variables 'a' and 'b' with predefined values.
+     */
+    static {
         System.out.println("I am in static block...");
         a = 5;
         b = a * 10;
     }
 
+    /**
+     * The main method is the entry point of the program.
+     * It demonstrates the usage of the class by creating objects and accessing static variables.
+     *
+     * @param args The command line arguments (not used in this example).
+     */
     public static void main(String[] args) {
+        // Create an object of the class to trigger static block execution.
         StaticInit obj = new StaticInit();
 
-        System.out.println(StaticInit.a);
-        System.out.println(StaticInit.b);
+        // Display the values of static variables 'a' and 'b'.
+        System.out.println("Value of 'a': " + StaticInit.a);
+        System.out.println("Value of 'b': " + StaticInit.b);
 
+        // Modify the value of 'b' and display the updated values.
         StaticInit.b += 12;
 
+        // Create another object to show that static block is not executed again.
         StaticInit obj1 = new StaticInit();
-        System.out.println(StaticInit.a);
-        System.out.println(StaticInit.b);
+        System.out.println("Updated value of 'a': " + StaticInit.a);
+        System.out.println("Updated value of 'b': " + StaticInit.b);
     }
 }
