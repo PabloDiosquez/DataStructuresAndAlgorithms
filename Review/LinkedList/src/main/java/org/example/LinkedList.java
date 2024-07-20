aa0cebfe50765350a238eb1d646745d49a0cb4ca
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-07-20 14:30:47 -0300

    'Linked List' was updated

--- Review/LinkedList/src/main/java/org/example/LinkedList.java
+++ Review/LinkedList/src/main/java/org/example/LinkedList.java
@@ -17,16 +17,36 @@ public class LinkedList {
         }
     }
 
+    public void insertAt(int data, int index){
+        if(index == 0){
+            insertAtStart(data);
+        } else {
+            Node node = new Node(data, null);
+            int count = 0;
+            Node current = head;
+            while(count < index-1){
+                current = current.getNext();
+                count++;
+            }
+            node.setNext(current.getNext());
+            current.setNext(node);
+        }
+    }
+
+    public void insertAtStart(int data){
+        head = new Node(data, head);
+    }
+
     public void show(){
-        if(head != null){
+        if(head == null){
+            System.out.println("The list is empty :(");
+        } else {
             Node current = this.head;
             while(current.getNext() != null){
                 System.out.print(current + " -> ");
                 current = current.getNext();
             }
             System.out.print(current);
-        } else {
-            System.out.println("The list is empty :(");
         }
     }
 
--- Review/LinkedList/src/main/java/org/example/Main.java
+++ Review/LinkedList/src/main/java/org/example/Main.java
@@ -6,7 +6,10 @@ public class Main {
         list.insert(12);
         list.insert(1);
         list.insert(21);
-
+        //list.insertAtStart(14);
+        list.show();
+        list.insertAt(100, 3);
+        System.out.println();
         list.show();
     }
 }
