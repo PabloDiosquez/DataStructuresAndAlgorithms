6682e2df2f52fa8112225d6f4f0877dac602336a
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-07-22 16:26:41 -0300

    'Stack' was updated

--- Review/Stack/src/main/java/org/example/LinkedStack.java
+++ Review/Stack/src/main/java/org/example/LinkedStack.java
@@ -1,4 +1,39 @@
 package org.example;
 
 public class LinkedStack {
+    private Node top;
+    private int size;
+
+    public LinkedStack() {
+        size = 0;
+    }
+
+    public void push(int data){
+        top = new Node(data, top);
+        size++;
+    }
+    public int pop() throws Exception {
+        if(this.isEmpty()){
+            throw new Exception("The stack is empty.");
+        }
+        int data = top.getData();
+        top = top.getNext();
+        size--;
+        return data;
+    }
+
+    public int peek() throws Exception {
+        if(this.isEmpty()){
+            throw new Exception("The stack is empty.");
+        }
+        return top.getData();
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
 }
