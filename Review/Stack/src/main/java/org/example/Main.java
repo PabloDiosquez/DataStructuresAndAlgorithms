4f1e1cbca17838641f8666d70b9cf5dd4df67b2f
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-07-23 13:59:54 -0300

    'Stack' was updated

--- Review/Stack/src/main/java/org/example/Main.java
+++ Review/Stack/src/main/java/org/example/Main.java
@@ -4,6 +4,18 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws Exception {
+        CustomStack stack = new CustomStack(5);
 
+        stack.push(12);
+        stack.push(1);
+        stack.push(21);
+        stack.push(22);
+        stack.push(34);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
     }
 }
---
+++ Review/Stack/src/main/java/org/example/StackException.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class StackException extends Exception{
+    public StackException(String message){
+        super(message);
+    }
+}
