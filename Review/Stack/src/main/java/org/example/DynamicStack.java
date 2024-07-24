0096f0a04763067425f526653cb921972252f021
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-07-24 10:36:33 -0300

    'Queue' was updated

--- Review/Stack/src/main/java/org/example/DynamicStack.java
+++ Review/Stack/src/main/java/org/example/DynamicStack.java
@@ -1,16 +1,29 @@
 package org.example;
 
-import java.util.Arrays;
+/**
+ * The DynamicStack class represents a stack data structure that can dynamically resize when it becomes full.
+ * It extends the CustomStack class and overrides the push method to allow dynamic resizing.
+ */
+public class DynamicStack extends CustomStack {
 
-public class DynamicStack extends CustomStack{
-    public DynamicStack(){
+    /**
+     * Constructs a DynamicStack with the default size.
+     */
+    public DynamicStack() {
         super();
     }
 
+    /**
+     * Pushes an item onto the top of this stack.
+     * If the stack is full, it doubles the size of the stack before pushing the item.
+     *
+     * @param item the item to be pushed onto this stack
+     * @return {@code true} if the item was successfully pushed onto the stack
+     */
     @Override
     public boolean push(int item) {
-        if(isFull()){
-            int[] temp = new int[2*data.length];
+        if (isFull()) {
+            int[] temp = new int[2 * data.length];
             System.arraycopy(data, 0, temp, 0, data.length);
             data = temp;
         }
