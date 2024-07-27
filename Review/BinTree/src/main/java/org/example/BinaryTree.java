bb802c56bb5187bc89716f317752626d0decf3e1
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-07-27 20:18:37 -0300

    'Binary Trees' was updated

--- Review/BinTree/src/main/java/org/example/BinaryTree.java
+++ Review/BinTree/src/main/java/org/example/BinaryTree.java
@@ -18,22 +18,41 @@ public class BinaryTree {
     private Node root;
 
     public BinaryTree() {
+        root = null;
     }
 
     public void populate(Scanner scanner){
         System.out.println("Enter the root node: ");
-        Node root = new Node(scanner.nextInt());
+        root = new Node(scanner.nextInt());
         populate(scanner, root);
     }
 
     private void populate(Scanner scanner, Node node) {
         System.out.println("Do you want to enter left of " + node.value);
-        boolean left = scanner.nextBoolean();
-
-        if(left){
+        if(scanner.nextBoolean()){
             System.out.println("Enter the value: ");
             node.left = new Node(scanner.nextInt());
             populate(scanner, node.left);
         }
+
+        System.out.println("Do you want to enter right of " + node.value);
+        if(scanner.nextBoolean()){
+            System.out.println("Enter the value: ");
+            node.right = new Node(scanner.nextInt());
+            populate(scanner, node.right);
+        }
+    }
+
+    public void display(){
+        display(root, " ");
+    }
+
+    private void display(Node node, String indent) {
+        if(node == null){
+            return;
+        }
+        System.out.println(indent + node);
+        display(node.left, indent + "\t");
+        display(node.right, indent + "\t");
     }
 }
