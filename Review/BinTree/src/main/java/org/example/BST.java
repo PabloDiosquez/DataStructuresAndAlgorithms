ccff9ee6c6425f42e1e73a9e245a89a6be178fe5
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-07-29 14:31:46 -0300

    'BST' was updated

---
+++ Review/BinTree/src/main/java/org/example/BST.java
@@ -0,0 +1,52 @@
+package org.example;
+
+public class BST {
+    private class Node{
+        private final int value;
+
+        private Node left;
+        private Node right;
+        private int height;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + value +
+                    '}';
+        }
+    }
+
+    private Node root;
+
+    public int height(Node node){
+        if(node == null){
+            return -1;
+        }
+        return node.height;
+    }
+
+    public boolean isEmpty(){
+        return root == null;
+    }
+
+    public void display(){
+        display(root, "Root node: ");
+    }
+
+    private void display(Node node, String details) {
+        if(node == null){
+            return;
+        }
+        System.out.println(details + node.getValue());
+        display(node.left, "Left child of " + node.getValue() + ": ");
+        display(node.right, "Right child of " + node.getValue() + ": ");
+    }
+
+}
