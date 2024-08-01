c6dcc5d8db5385df6a7e332ef3d48390903857f2
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-08-01 16:30:22 -0300

    'Custom LL' was updated

--- Review/LinkedList/src/main/java/org/example/CustomLinkedList.java
+++ Review/LinkedList/src/main/java/org/example/CustomLinkedList.java
@@ -1,16 +1,29 @@
 package org.example;
 
-public class SingleLinkedList {
+public class CustomLinkedList {
 
     private class Node{
         private int value;
         private Node next;
         public Node(int value) {
-            this.value = value;
+            this(value, null);
         }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
         @Override
         public String toString() {
             return String.format("%s", value);
         }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public CustomLinkedList() {
     }
 }
