41086082c3ca60e5d80aefb8b1316c43ad533869
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-08-05 15:39:25 -0300

    'HashMap' was updated

--- Review/HashMap/src/main/java/org/example/MapUsingHash.java
+++ Review/HashMap/src/main/java/org/example/MapUsingHash.java
@@ -26,4 +26,17 @@ public class MapUsingHash {
     public MapUsingHash() {
         this.entities = new Entity[100];
     }
+
+    public void put(String key, String value){
+        int hash = Math.abs(key.hashCode() % entities.length);
+        entities[hash] = new Entity(key, value);
+    }
+
+    public String get(String key){
+        int hash = Math.abs(key.hashCode() % entities.length);
+        if(entities[hash] != null && entities[hash].getKey().equals(key)){
+            return entities[hash].getValue();
+        }
+        return null;
+    }
 }
