8ba266d4ac5824b061640d0c2fd89debfa7da301
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-08-05 09:32:34 -0300

    'Heap' was updated

--- Review/Heap/src/main/java/org/example/Heap.java
+++ Review/Heap/src/main/java/org/example/Heap.java
@@ -3,26 +3,45 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A generic heap implementation.
+ *
+ * @param <T> the type of elements in this heap, must extend Comparable
+ */
 public class Heap<T extends Comparable<T>> {
     private ArrayList<T> list;
 
+    /**
+     * Constructs an empty heap.
+     */
     public Heap() {
         this.list = new ArrayList<>();
     }
 
-    public void insert(T value){
+    /**
+     * Inserts a value into the heap.
+     *
+     * @param value the value to be inserted
+     */
+    public void insert(T value) {
         list.add(value);
-        upHeap(list.size()-1);
+        upHeap(list.size() - 1);
     }
 
+    /**
+     * Removes and returns the smallest value from the heap.
+     *
+     * @return the smallest value in the heap
+     * @throws Exception if the heap is empty
+     */
     public T remove() throws Exception {
-        if(list.isEmpty()){
+        if (list.isEmpty()) {
             throw new Exception("Removing from an empty list!");
         }
 
         T removed = list.get(0);
-        T last = list.remove(list.size()-1);
-        if(!list.isEmpty()){
+        T last = list.remove(list.size() - 1);
+        if (!list.isEmpty()) {
             list.set(0, last);
             downHeap(0);
         }
@@ -30,10 +49,15 @@ public class Heap<T extends Comparable<T>> {
         return removed;
     }
 
-    //O(N * logN)
-    public ArrayList<T> heapSort() throws Exception{
+    /**
+     * Performs heap sort and returns a sorted list of the heap's elements.
+     *
+     * @return a sorted list of the heap's elements
+     * @throws Exception if the heap is empty
+     */
+    public ArrayList<T> heapSort() throws Exception {
         ArrayList<T> data = new ArrayList<>();
-        while(!list.isEmpty()){
+        while (!list.isEmpty()) {
             data.add(this.remove());
         }
         return data;
@@ -44,46 +68,46 @@ public class Heap<T extends Comparable<T>> {
         int left = left(index);
         int right = right(index);
 
-        if(left < list.size() && list.get(min).compareTo(list.get(left)) > 0){
+        if (left < list.size() && list.get(min).compareTo(list.get(left)) > 0) {
             min = left;
         }
 
-        if(right < list.size() && list.get(min).compareTo(list.get(right)) > 0){
+        if (right < list.size() && list.get(min).compareTo(list.get(right)) > 0) {
             min = right;
         }
 
-        if(min != index){
+        if (min != index) {
             swap(index, min);
             downHeap(min);
         }
     }
 
     private void upHeap(int index) {
-        if(index == 0){
+        if (index == 0) {
             return;
         }
         int p = parent(index);
-        if(list.get(index).compareTo(list.get(p)) < 0){
+        if (list.get(index).compareTo(list.get(p)) < 0) {
             swap(index, p);
             upHeap(p);
         }
     }
 
-    private void swap(int first, int second){
+    private void swap(int first, int second) {
         T temp = list.get(first);
         list.set(first, list.get(second));
         list.set(second, temp);
     }
 
-    private int parent(int index){
-        return (index-1) /2;
+    private int parent(int index) {
+        return (index - 1) / 2;
     }
 
-    private int left(int index){
-        return 2*index + 1;
+    private int left(int index) {
+        return 2 * index + 1;
     }
 
-    private int right(int index){
-        return 2*index + 2;
+    private int right(int index) {
+        return 2 * index + 2;
     }
 }
