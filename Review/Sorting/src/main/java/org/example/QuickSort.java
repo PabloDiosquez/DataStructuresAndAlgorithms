d339f5d67e9cd975b37103515adbd766053b9cf1
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-08-06 15:20:57 -0300

    'QuickSort' was updated

--- Review/Sorting/src/main/java/org/example/QuickSort.java
+++ Review/Sorting/src/main/java/org/example/QuickSort.java
@@ -4,6 +4,35 @@ package org.example;
 //In-place
 public class QuickSort {
 
+    public static void sort(int[] arr, int low, int high){
+        if(low >= high){
+            return;
+        }
+
+        int start = low;
+        int end = high;
+        int middle = start + (end - start) / 2;
+        int pivot = arr[middle];
+
+        while(start <= end){
+            while(arr[start] < pivot){
+                start++;
+            }
+
+            while(arr[end] > pivot){
+                end--;
+            }
+
+            if(start <= end){
+                swap(arr, start, end);
+                start++;
+                end--;
+            }
+            // Recursion part
+            sort(arr, low, end);
+            sort(arr, start, high);
+        }
+    }
 
 
     private static void swap(int[] arr, int first, int second){
