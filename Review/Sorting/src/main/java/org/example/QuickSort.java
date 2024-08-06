9d562a6cd6296bc167b55a86405fbc0f4a78456a
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-08-06 15:06:38 -0300

    'Quick Sort' was updated

--- Review/Sorting/src/main/java/org/example/QuickSort.java
+++ Review/Sorting/src/main/java/org/example/QuickSort.java
@@ -1,4 +1,14 @@
 package org.example;
 
+//Not stable
+//In-place
 public class QuickSort {
+
+
+
+    private static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
 }
