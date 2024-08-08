05b9132671c11018b1365d3496237cdfc5624ba7
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-08-08 16:56:57 -0300

    'Insertion Sort' was updated

--- Review/Sorting/src/main/java/org/example/InsertionSort.java
+++ Review/Sorting/src/main/java/org/example/InsertionSort.java
@@ -1,25 +1,32 @@
 package org.example;
 
-// Stable.
-// Used for smaller values of N. Works good when array is
-// partially sorted.
-// Best case: O(N)
-// Worst case: O(N2)
+// InsertionSort is a class that provides a static method to sort an array of integers
+// using the insertion sort algorithm.
+//
+// Insertion sort is a stable sorting algorithm that works well for smaller arrays
+// or arrays that are already partially sorted.
+//
+// Characteristics:
+// - Stable: Yes
+// - Best case time complexity: O(N)
+// - Worst case time complexity: O(N^2)
+// - Use case: Suitable for smaller arrays or arrays that are partially sorted
 public class InsertionSort {
 
-    public static void sort(int[] arr){
-        int i = 0;
-        while(i < arr.length-1){
-            int j = i+1;
-            while (j > 0 && arr[j] < arr[j-1]){
-                swap(arr, j, j-1);
-                j--;
+    // Sorts the specified array of integers using the insertion sort algorithm.
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] >= arr[j - 1]) {
+                    break;
+                }
+                swap(arr, j, j - 1);
             }
-            i++;
         }
     }
 
-    private static void swap(int[] arr, int first, int second){
+    // Swaps two elements in the specified array.
+    private static void swap(int[] arr, int first, int second) {
         int temp = arr[first];
         arr[first] = arr[second];
         arr[second] = temp;
