017e15785100024f7bf4e964321138d65b2f6108
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-08-08 16:49:01 -0300

    'Insertion Sort' was updated

--- Review/Sorting/src/main/java/org/example/InsertionSort.java
+++ Review/Sorting/src/main/java/org/example/InsertionSort.java
@@ -1,5 +1,27 @@
 package org.example;
 
+// Stable.
+// Used for smaller values of N. Works good when array is
+// partially sorted.
+// Best case: O(N)
+// Worst case: O(N2)
 public class InsertionSort {
 
+    public static void sort(int[] arr){
+        int i = 0;
+        while(i < arr.length-1){
+            int j = i+1;
+            while (j > 0 && arr[j] < arr[j-1]){
+                swap(arr, j, j-1);
+                j--;
+            }
+            i++;
+        }
+    }
+
+    private static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
 }
