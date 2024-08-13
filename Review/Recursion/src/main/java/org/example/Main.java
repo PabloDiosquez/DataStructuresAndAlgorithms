3fb9a7ffc05921dd5501481daba14b0a1d5dfbea
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-08-13 16:38:59 -0300

    'Recursion' was updated

--- Review/Recursion/src/main/java/org/example/Main.java
+++ Review/Recursion/src/main/java/org/example/Main.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        message(5);
+    }
 
+    static void message(){
+        System.out.println("Hello World!");
+    }
+
+    static void message(int times){
+        if(times == 0){
+            return;
+        }
+        System.out.println("Hello World!");
+        message(times-1);
     }
 }
