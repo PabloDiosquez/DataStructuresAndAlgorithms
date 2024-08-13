facf0074f0926999765d35d6ce82c8fae8eb193d
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-08-13 17:37:29 -0300

    'Recursion' was updated

--- Review/Recursion/src/main/java/org/example/NumbersExample.java
+++ Review/Recursion/src/main/java/org/example/NumbersExample.java
@@ -1,16 +1,31 @@
 package org.example;
 
+/**
+ * A class to demonstrate printing numbers from 1 to a specified number.
+ */
 public class NumbersExample {
 
-    public static void printsNumbers(int number){
-        printsNumbersFrom(number, 1);
+    /**
+     * Prints numbers from 1 up to the specified number.
+     *
+     * @param number The number up to which to print. Must be greater than or equal to 1.
+     */
+    public static void printNumbers(int number) {
+        printNumbersFrom(number, 1);
     }
 
-    private static void printsNumbersFrom(int number, int start){
-        if(start > number){
+    /**
+     * Recursively prints numbers from the start value up to the specified number.
+     *
+     * @param number The number up to which to print.
+     * @param start  The starting number from which to begin printing.
+     */
+    private static void printNumbersFrom(int number, int start) {
+        if (start == number) {
+            System.out.print(start);
             return;
         }
         System.out.print(start + " ");
-        printsNumbersFrom(number, start+1);
+        printNumbersFrom(number, start + 1);
     }
 }
