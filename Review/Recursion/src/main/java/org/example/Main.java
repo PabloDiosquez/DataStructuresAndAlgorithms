1018453c4339fc6fac960fec7f628a7eea1d47bd
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-08-13 16:44:45 -0300

    'Recursion' was updated

--- Review/Recursion/src/main/java/org/example/Main.java
+++ Review/Recursion/src/main/java/org/example/Main.java
@@ -1,19 +1,36 @@
 package org.example;
 
+/**
+ * Main class to demonstrate printing a message multiple times.
+ */
 public class Main {
+
+    /**
+     * Entry point of the program. Calls the method to print a message a specified number of times.
+     *
+     * @param args Command line arguments (not used).
+     */
     public static void main(String[] args) {
-        message(5);
+        printMessage(5);
     }
 
-    static void message(){
+    /**
+     * Prints "Hello World!" once.
+     */
+    static void printMessage() {
         System.out.println("Hello World!");
     }
 
-    static void message(int times){
-        if(times == 0){
+    /**
+     * Prints "Hello World!" a specified number of times.
+     *
+     * @param times The number of times to print the message. If 0, no message is printed.
+     */
+    static void printMessage(int times) {
+        if (times == 0) {
             return;
         }
         System.out.println("Hello World!");
-        message(times-1);
+        printMessage(times - 1);
     }
 }
