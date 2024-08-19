16d44fd682ae15f2d10872d80f2020a2a219a389
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-08-19 13:59:40 -0300

    'Recursion' was updated

--- Review/Recursion/src/main/java/org/example/Fibonacci.java
+++ Review/Recursion/src/main/java/org/example/Fibonacci.java
@@ -1,11 +1,20 @@
 package org.example;
 
+/**
+ * The Fibonacci class provides a method to calculate the nth Fibonacci number.
+ */
 public class Fibonacci {
 
-    public static int nthFibonacciNumber(int n){
-        if(n <= 1){
+    /**
+     * Computes the nth Fibonacci number using recursion.
+     *
+     * @param n The position of the Fibonacci number to compute. Must be a non-negative integer.
+     * @return The nth Fibonacci number.
+     */
+    public static int nthFibonacciNumber(int n) {
+        if (n <= 1) {
             return n;
         }
-        return nthFibonacciNumber(n-1) + nthFibonacciNumber(n-2);
+        return nthFibonacciNumber(n - 1) + nthFibonacciNumber(n - 2);
     }
 }
