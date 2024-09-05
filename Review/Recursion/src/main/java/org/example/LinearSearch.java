4ace2b47251df01ff7620d306d3589ba1680ce52
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-09-04 21:58:53 -0300

    'Recursion' was updated

--- Review/Recursion/src/main/java/org/example/LinearSearch.java
+++ Review/Recursion/src/main/java/org/example/LinearSearch.java
@@ -1,9 +1,27 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class LinearSearch {
     public static void main(String[] args) {
-        int[] arr = new int[] {3, 5, 12, 6, 7, 0, 21,};
-        System.out.println(search(arr, 4));
+        int[] arr = new int[] {3, 5, 12, 4, 7, 0, 4,};
+        // System.out.println(search(arr, 4));
+        System.out.println(getElementPositions(arr, 4));
+    }
+
+    static ArrayList<Integer> getElementPositions(int[] arr, int key){
+        ArrayList<Integer> positions = new ArrayList<>();
+        return helperGetElementPositions(arr, key, positions, 0);
+    }
+
+    private static ArrayList<Integer> helperGetElementPositions(int[] arr, int key, ArrayList<Integer> positions, int s) {
+        if(s == arr.length){
+            return positions;
+        }
+        if(arr[s] == key){
+            positions.add(s);
+        }
+        return helperGetElementPositions(arr, key, positions, s+1);
     }
 
     static int search(int[] arr, int key){
